09c101ff92c4e0eb21dbbd91bc8bc1dab536eab8
Vina <vina.marieluce@gmail.com>
2022-12-01 11:11:11 +0100

    Portfolio

---
+++ assets/js/burger-menu.js
@@ -0,0 +1,11 @@
+// SELECTION DES ELEMENTS DANS LE DOM
+
+const burgerMenu = document.querySelector('#burger-menu');
+const overlay = document.querySelector('#menu');
+
+// AJOUT EVENEMENT ONCLICK POUR OUVRIR ET FERMER LE BURGER MENU
+
+burgerMenu.onclick = function() {
+    this.classList.toggle("ferme");
+    overlay.classList.toggle("overlay");
+}
